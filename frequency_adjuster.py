9b1d723248802fca42c9f9468bbdf572f070a9e9
Linus Tolke <ltolke@gmail.com>
2023-06-06 15:27:48 +0200

    Move documentation of the logic into the file itself

--- frequency_adjuster.py
+++ frequency_adjuster.py
@@ -1,5 +1,62 @@
 #!/usr/bin/env python3
 
+"""Frequency-adjusting filter
+
+I have noticed that sometimes the same spot shows up with different
+frequencies.  Is this because the spotters are varying a little in
+frequency.  If so, it should be possible to calculate how much each
+spotter deviates from the others and then adjust each spot's frequency
+based on that.
+
+Important thing to check for this to work:
+That the program can use more precise frequencies.
+
+
+Assumptions
+
+Spotters deviate the same amount on the same band.  If the move around
+a little, that is a slow move, comparable to a frequency drift.
+
+Spotted station most of the time stay on the same frequency.  If they
+change the frequency enough to be detected by any spotter that is
+handled as QSY-ing.
+
+
+Current Limitations
+
+Each spotted station can only work on one frequency at the time.
+
+
+Logic
+
+To calculate the spotter offset, for each spot, calculate the
+difference between the spot's frequency from that spotter and the some
+average of that call based on information on all spotters. Adjust the
+offset of the spotter by a small amount.
+
+Show the spot with the adjusted frequency.  Add digits to the
+frequency to add precision.
+
+To adjust against drift of all spotters, for each spot, calculate how
+much the frequency is adjusted and adjust by a small amount towards 0.
+
+
+Validation
+
+Output spotter offsets and examine the curves.
+
+Define a metric on how "wrong" the frequencies are and compare it
+against the non-complensated frequencies (for every spot).
+
+Metric: For each spot, if the same call has been spotted recently
+(last 10 minutes), sum the difference in the frequency since last
+spot. Ignore big changes >0.3kHz. Different spotters may typically add
+0.1 if there is a drift. Small QSYs <0.3kHz will be counted once. For
+the adjusted frequencies, calculate the jumps in frequencies adjusted
+by the offset of the spotter.
+
+"""
+
 import math
 import re
 import socket
@@ -43,28 +100,29 @@ def top_adjustments(d):
                   reverse=True)[:3]
 
 
-count = 0
-dxes = dict()
-spotter_adjustments = dict() # (str, band) => float
-total = 0
-for spot in dxcluster.spots(CALL, (HOST, PORT)):
-    count = count + 1
-    if count % 100 == 0:
-        print(count, "spots filtered", "total", len(dxes), "monitored")
-        print(top_adjustments(spotter_adjustments))
-        print("total drift:", total)
-    spotter_band = (spot.spotter, band(spot.freq))
-    if spotter_band not in spotter_adjustments:
-        spotter_adjustments[spotter_band] = 0.0
-    adjusted_frequency_1 = spot.freq + spotter_adjustments[spotter_band]
-    if spot.dx not in dxes:
-        dxes[spot.dx] = SpotAverage(spot.dx)
-    adjusted_frequency_2 = dxes[spot.dx].adjust(spot.spotter, adjusted_frequency_1)
-    freq_diff = adjusted_frequency_2 - spot.freq
-    total += freq_diff
-    adjusted_frequency_3 = adjusted_frequency_2 - total / 1000
-    spotter_adjustments[spotter_band] += (adjusted_frequency_2 - adjusted_frequency_1) / 10
-    spotout = spot.spotter + ":"
-    print(f"DX de {spotout:<15s}",
-          f"{adjusted_frequency_2:>9.3f}",
-          f"{spot.dx:<14}", spot.rest)
+if __name__ == "__main__":
+    count = 0
+    dxes = dict()
+    spotter_adjustments = dict() # (str, band) => float
+    total = 0
+    for spot in dxcluster.spots(CALL, (HOST, PORT)):
+        count = count + 1
+        if count % 100 == 0:
+            print(count, "spots filtered", "total", len(dxes), "monitored")
+            print(top_adjustments(spotter_adjustments))
+            print("total drift:", total)
+        spotter_band = (spot.spotter, band(spot.freq))
+        if spotter_band not in spotter_adjustments:
+            spotter_adjustments[spotter_band] = 0.0
+        adjusted_frequency_1 = spot.freq + spotter_adjustments[spotter_band]
+        if spot.dx not in dxes:
+            dxes[spot.dx] = SpotAverage(spot.dx)
+        adjusted_frequency_2 = dxes[spot.dx].adjust(spot.spotter, adjusted_frequency_1)
+        freq_diff = adjusted_frequency_2 - spot.freq
+        total += freq_diff
+        adjusted_frequency_3 = adjusted_frequency_2 - total / 1000
+        spotter_adjustments[spotter_band] += (adjusted_frequency_2 - adjusted_frequency_1) / 10
+        spotout = spot.spotter + ":"
+        print(f"DX de {spotout:<15s}",
+              f"{adjusted_frequency_2:>9.3f}",
+              f"{spot.dx:<14}", spot.rest)
